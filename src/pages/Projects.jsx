2ea09cda46da4bb1fee17e2c07ab43446d76b4d9
Tufan Mandal <tufanm843@gmail.com>
2025-02-22 13:01:08 +0530

    Add project Section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BACKEND from "../../constrants";
 
 function Projects() {
-  return <div>Projects</div>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND}/project`)
+      .then((res) => {
+        setProjects(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div className="container">
+        <div className="row">
+          {projects.map((project) => (
+            <div className="col-lg-3">
+              <div className="banner">
+                <img src={project.banner} alt="banner" />
+              </div>
+              <Link>
+                <h5>{project.title}</h5>
+              </Link>
+              <p>{project.smallDescription}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
